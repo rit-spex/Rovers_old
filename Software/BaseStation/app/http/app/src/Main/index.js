7b6f6bed36d51bf6cc05df71540ab71f5d6e4418
Thomas Hall <tjh2822@g.rit.edu>
2019-10-05 20:21:00 -0400

    Added arm, rover view skeleton

--- Software/BaseStation/app/http/app/src/Main/index.js
+++ Software/BaseStation/app/http/app/src/Main/index.js
@@ -8,20 +8,7 @@ import Home from '../Home'
 class Main extends Component {
   render() {
     return (
-      <main>
-        <Security 
-          issuer={'https://dev-509836.oktapreview.com/oauth2/default'}
-          client_id={'0oagcbm1o6GTTB9Da0h7'}
-          redirect_uri={'http://localhost:3000/implicit/callback'}
-          scope={['openid', 'profile', 'email']}>
-          
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route path="/implicit/callback" component={ImplicitCallback} />
-            <SecureRoute path="/home" component={Home} />
-          </Switch>
-        </Security>
-      </main>
+      <p>Main</p>
     );
   }
 }
---
+++ Software/BaseStation/app/http/app/src/RoverView/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import { Redirect } from 'react-router-dom'
+import { withAuth } from '@okta/okta-react';
+
+class RoverView extends React.Component {
+
+  render() {
+    return (
+      <p>RoverView</p>
+    )
+  }
+}
+
+export default RoverView
